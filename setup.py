005acbec7274f6dd01af52ca3aca784bb802d2c9
alex.xi <alex.x.xi@juwai.com>
2019-05-07 11:52:06 +0800

    minor update

--- setup.py
+++ setup.py
@@ -2,14 +2,10 @@ from distutils.core import setup
 import weibo
 
 kw = dict(
-    name='sinaweibopy-ng',
+    name='sinaweibopy3',
     version=weibo.__version__,
     description='Sina Weibo OAuth 2 API Python SDK',
     long_description=open('README', 'r').read(),
-    author='ifanr',
-    author_email='ifanrx@ifanr.com',
-    url='https://github.com/ifanrx/sinaweibopy',
-    download_url='https://github.com/ifanrx/sinaweibopy',
     py_modules=['weibo'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -17,7 +13,7 @@ kw = dict(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python 3.6',
         'Topic :: Internet',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
